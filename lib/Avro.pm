042a9c63b89732e3a8620320ab7ac62f0bd46d19
Yann Kerherve <yann@cyberion.net>
2010-05-27 21:00:20 -0700

    pmsetup boilerplate

---
+++ lib/Avro.pm
@@ -0,0 +1,37 @@
+package Avro;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Avro - Modern Data Serializer
+
+=head1 SYNOPSIS
+
+  use Avro;
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Yann Kerherve E<lt>yannk@cpank.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2010- Yann Kerherve
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
