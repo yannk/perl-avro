ea08064e934bc8eb7656d0f42d764cc588f78346
Yann Kerherve <yann@cyberion.net>
2010-05-28 20:56:14 -0700

    Adds most of the Schema capabilities
    * It misses doc and other ignored attributes
    * Default values are sometimes not correctly validated, but it looks
      like I'm doing a much better job that most other implementations
      anyways

--- lib/Avro/Schema.pm
+++ lib/Avro/Schema.pm
@@ -2,10 +2,11 @@ package Avro::Schema;
 use strict;
 use warnings;
 
-use JSON::XS;
+use Bit::Vector();
+use JSON::XS();
 use Try::Tiny;
 
-my $json = JSON::XS->new->allow_nonref(1);
+my $json = JSON::XS->new->allow_nonref;
 
 sub parse {
     my $schema      = shift;
@@ -23,29 +24,67 @@ sub parse {
     return $schema->parse_struct($struct, $names);
 }
 
+sub to_string {
+    my $class = shift;
+    my $struct = shift;
+    return $json->encode($struct);
+}
+
 sub parse_struct {
     my $schema = shift;
     my $struct = shift;
     my $names = shift || {};
 
-    ## A JSON object
+    ## 1.3.2 A JSON object
     if (ref $struct eq 'HASH') {
-        my $type = $struct->{type};
+        my $type = $struct->{type}
+            or throw Avro::Schema::Error::ParseError("type is missing");
         if ( Avro::Schema::Primitive->is_valid($type) ) {
             return Avro::Schema::Primitive->new(type => $type);
         }
-        return Avro::Schema::Record->new(struct => $struct, names => $names);
+        if ($type eq 'record') {
+            return Avro::Schema::Record->new(
+                struct => $struct,
+                names => $names,
+            );
+        }
+        elsif ($type eq 'enum') {
+            return Avro::Schema::Enum->new(struct => $struct, names => $names);
+        }
+        elsif ($type eq 'array') {
+            return Avro::Schema::Array->new(
+                struct => $struct,
+                names => $names,
+            );
+        }
+        elsif ($type eq 'map') {
+            return Avro::Schema::Map->new(
+                struct => $struct,
+                names => $names,
+            );
+        }
+        elsif ($type eq 'fixed') {
+            return Avro::Schema::Fixed->new(
+                struct => $struct,
+                names => $names,
+            );
+        }
+        else {
+            throw Avro::Schema::Error::ParseError("unknown type: $type");
+        }
     }
-    ## A JSON array, representing a union of embedded types.
+    ## 1.3.2 A JSON array, representing a union of embedded types.
     elsif (ref $struct eq 'ARRAY') {
         return Avro::Schema::Union->new(struct => $struct, names => $names);
     }
-    ## A JSON string, naming a defined type.
+    ## 1.3.2 A JSON string, naming a defined type.
     else {
         my $type = $struct;
+        ## It's one of our custom defined type
         if (exists $names->{$type}) {
             return $names->{$type};
         }
+        ## It's a primitive type
         else {
             return Avro::Schema::Primitive->new(type => $type);
         }
@@ -67,7 +106,6 @@ sub new {
     my $schema = bless {
         type => $type,
     }, $class;
-#    $schema->parse($struct);
     return $schema;
 }
 
@@ -76,9 +114,16 @@ sub type {
     return $schema->{type};
 }
 
+sub to_string {
+    my $schema = shift;
+    my $known_names = shift || {};
+    return Avro::Schema->to_string($schema->to_struct($known_names));
+}
+
 package Avro::Schema::Primitive;
 our @ISA = qw/Avro::Schema::Base/;
 use Carp;
+use Regexp::Common qw/number/;
 
 my %PrimitiveType = map { $_ => 1 } qw/
     null
@@ -101,6 +146,9 @@ sub new {
     my $type = $param{type}
         or croak "Schema must have a type";
 
+    throw Avro::Schema::Error::ParseError("Not a primitive type $type")
+        unless $class->is_valid($type);
+
     if (! exists $Singleton{ $type } ) {
         my $schema = $class->SUPER::new( type => $type );
         $Singleton{ $type } = $schema;
@@ -112,14 +160,65 @@ sub is_valid {
     return $PrimitiveType{ $_[1] || "" };
 }
 
+## takes a default value and return an array:
+## ($bool, $normalized) where $bool is true if the default value is valid
+## and $normalized is the normalized default value to use
+sub is_default_valid {
+    my $schema = shift;
+    my $default = shift;
+    my $type = $schema->{type};
+    if ($type eq 'null') {
+        return defined $default ? (0) : (1, undef);
+    }
+    if ($type eq 'boolean') {
+        return (1, $default ? 1 : 0);
+    }
+    unless (defined $default && length $default) {
+        return (0);
+    }
+    if ($type eq "bytes" or $type eq "string") {
+        return (1, $default);
+    }
+    if ($type eq 'int') {
+        no warnings;
+        my $packed_int = pack "l", $default;
+        my $unpacked_int = unpack "l", $packed_int;
+        return $unpacked_int eq $default ? (1, $default) : (0);
+    }
+    if ($type eq 'long') {
+        ## TODO: faster with pack Q if 64bit perl
+        my $dec;
+        eval {
+            my $vec = Bit::Vector->new_Dec(64, $default);
+            $dec = $vec->to_Dec;
+        };
+        return (0) if $@;
+        return ($dec eq $default) ? (1, $default) : (0);
+    }
+    if ($type eq 'float' or $type eq 'double') {
+        $default =~ /^$RE{num}{real}$/ ? return (1, $default) : (0);
+    }
+}
+
+sub to_struct {
+    my $schema = shift;
+    return $schema->type;
+}
+
 package Avro::Schema::Named;
 our @ISA = qw/Avro::Schema::Base/;
 use Scalar::Util;
 
+my %NamedType = map { $_ => 1 } qw/
+    record
+    enum
+    fixed
+/;
+
 sub new {
     my $class = shift;
     my %param = @_;
-    my $schema = bless {}, $class;
+    my $schema = $class->SUPER::new(%param);
 
     my $names  = $param{names} || {};
     my $struct = $param{struct} || {};
@@ -128,6 +227,10 @@ sub new {
 
     $class->add_name($names, $schema);
     return $schema;
+}
+
+sub is_valid {
+    return $NamedType{ $_[1] || "" };
 }
 
 sub add_name {
@@ -158,6 +261,8 @@ package Avro::Schema::Record;
 our @ISA = qw/Avro::Schema::Named/;
 use Scalar::Util;
 
+my %ValidOrder = map { $_ => 1 } qw/ascending descending ignore/;
+
 sub new {
     my $class = shift;
     my %param = @_;
@@ -166,9 +271,9 @@ sub new {
     my $schema = $class->SUPER::new(%param);
 
     my $fields = $param{struct}{fields}
-        or throw Arvo::Schema::Error::ParseError("Record must have Fields");
+        or throw Avro::Schema::Error::ParseError("Record must have Fields");
 
-    throw Arvo::Schema::Error::ParseError("Record.Fields must me an array")
+    throw Avro::Schema::Error::ParseError("Record.Fields must me an array")
         unless ref $fields eq 'ARRAY';
 
     my @fields;
@@ -182,14 +287,54 @@ sub new {
             unless defined $type && length $type;
 
         $type = Avro::Schema->parse_struct($type, $names);
-        my $field = { name => $name, type => $type };
-        ## TODO: default
-        Scalar::Util::weaken($field->{type});
+        my $f = { name => $name, type => $type };
+        #TODO: find where to weaken precisely
+        #Scalar::Util::weaken($field->{type});
 
-        push @fields, $field;
+        if (exists $field->{default}) {
+            my ($is_valid, $default) =
+                $type->is_default_valid($field->{default});
+            my $t = $type->type;
+            throw Avro::Schema::Error::ParseError(
+                "default value doesn't validate $t: '$field->{default}'"
+            ) unless $is_valid;
+            $f->{default} = $default;
+        }
+        if (my $order = $field->{order}) {
+            throw Avro::Schema::Error::ParseError(
+                "Order '$order' is not valid'"
+            ) unless $ValidOrder{$order};
+            $f->{order} = $order;
+        }
+
+        push @fields, $f;
     }
     $schema->{fields} = \@fields;
     return $schema;
+}
+
+sub field_to_struct {
+    my $field = shift;
+    my $known_names = shift || {};
+    my $type = $field->{type}->to_struct($known_names);
+    return { name => $field->{name}, type => $type };
+}
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+    ## consider that this record type is now known (will serialize differently)
+    my $fullname = $schema->fullname;
+    if ($known_names->{ $fullname }++) {
+        return $fullname;
+    }
+    return {
+        type => 'record',
+        name => $fullname,
+        fields => [
+            map { field_to_struct($_, $known_names) } @{ $schema->{fields} }
+        ],
+    };
 }
 
 sub fields {
@@ -200,25 +345,260 @@ sub fields {
 package Avro::Schema::Enum;
 our @ISA = qw/Avro::Schema::Named/;
 
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $schema = $class->SUPER::new(%param);
+    my $struct = $param{struct}
+        or throw Avro::Schema::Error::ParseError("Enum instantiation");
+    my $symbols = $struct->{symbols} || [];
+
+    unless (@$symbols) {
+        throw Avro::Schema::Error::ParseError("Enum needs at least one symbol");
+    }
+    my %symbols;
+    for (@$symbols) {
+        if (ref $_) {
+            throw Avro::Schema::Error::ParseError(
+                "Enum.symbol should be a string"
+            );
+        }
+        throw Avro::Schema::Error::ParseError("Duplicate symbol in Enum")
+            if $symbols{$_}++;
+    }
+    $schema->{symbols} = \%symbols;
+    return $schema;
+}
+
+sub is_default_valid {
+    my $schema = shift;
+    my $default = shift;
+    return (1, $default) if defined $default && $schema->{symbols}{$default};
+    return (0);
+}
+
+sub symbols {
+    my $schema = shift;
+    return [ keys %{ $schema->{symbols} } ];
+}
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+
+    my $fullname = $schema->fullname;
+    if ($known_names->{ $fullname }++) {
+        return $fullname;
+    }
+    return {
+        type => 'enum',
+        name => $schema->fullname,
+        symbols => [ keys %{ $schema->{symbols} } ],
+    };
+}
+
 package Avro::Schema::Array;
 our @ISA = qw/Avro::Schema::Base/;
+
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $schema = $class->SUPER::new(%param);
+
+    my $struct = $param{struct}
+        or throw Avro::Schema::Error::ParseError("Enum instantiation");
+
+    my $items = $struct->{items}
+        or throw Avro::Schema::Error::ParseError("Array must declare 'items'");
+
+    unless (defined $items && length $items) {
+        throw Avro::Schema::Error::ParseError(
+            "Array.items should be a string"
+        );
+    }
+    $schema->{items} = $items;
+    return $schema;
+}
+
+sub is_default_valid {
+    my $schema = shift;
+    my $default = shift;
+    return (1, $default) if $default && ref $default eq 'ARRAY';
+    return (0);
+}
+
+sub items {
+    my $schema = shift;
+    return $schema->{items};
+}
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+
+    return {
+        type => 'array',
+        items => $schema->{items},
+    };
+}
 
 package Avro::Schema::Map;
 our @ISA = qw/Avro::Schema::Base/;
 
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $schema = $class->SUPER::new(%param);
+
+    my $struct = $param{struct}
+        or throw Avro::Schema::Error::ParseError("Map instantiation");
+
+    my $values = $struct->{values};
+    unless (defined $values && length $values) {
+        throw Avro::Schema::Error::ParseError("Map must declare 'values'");
+    }
+    if (ref $values) {
+        throw Avro::Schema::Error::ParseError(
+            "Map.values should be a string"
+        );
+    }
+    $schema->{values} = $values;
+
+    return $schema;
+}
+
+sub is_default_valid {
+    my $schema = shift;
+    my $default = shift;
+    return (1, $default) if $default && ref $default eq 'HASH';
+    return (0);
+}
+
+sub values {
+    my $schema = shift;
+    return $schema->{values};
+}
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+
+    return {
+        type => 'map',
+        values => $schema->{values},
+    };
+}
+
 package Avro::Schema::Union;
 our @ISA = qw/Avro::Schema::Base/;
 
-sub parse_struct {
-    my $schema = shift;
-    my $types = shift;
-    my $names = shift || {};
-    for my $type (@$types) {
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $schema = $class->SUPER::new(%param);
+    my $union = $param{struct}
+        or throw Avro::Schema::Error::ParseError("Union.new needs a struct");
+
+    my $names = $param{names} ||= {};
+    my @schemas;
+    my %seen_types;
+    for my $struct (@$union) {
+        my $sch = Avro::Schema->parse_struct($struct, $names);
+        my $type = $sch->type;
+
+        ## 1.3.2 Unions may not contain more than one schema with the same
+        ## type, except for the named types record, fixed and enum. For
+        ## example, unions containing two array types or two map types are not
+        ## permitted, but two types with different names are permitted.
+        if (Avro::Schema::Named->is_valid($type)) {
+            $type = $sch->fullname; # resolve Named types to their name
+        }
+        ## XXX: I could define &type_name doing the correct resolution for all classes
+        if ($seen_types{ $type }++) {
+            throw Avro::Schema::Error::ParseError(
+                "$type is present more than once in the union"
+            )
+        }
+        ## 1.3.2 Unions may not immediately contain other unions.
+        if ($type eq 'union') {
+            throw Avro::Schema::Error::ParseError(
+                "Cannot embed unions in union"
+            );
+        }
+        push @schemas, $sch;
     }
+    $schema->{schemas} = \@schemas;
+
+    return $schema;
+}
+
+sub is_default_valid { (0) }
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+    return [ map { $_->to_struct($known_names) } @{$schema->{schemas}} ];
 }
 
 package Avro::Schema::Fixed;
 our @ISA = qw/Avro::Schema::Named/;
+
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $schema = $class->SUPER::new(%param);
+
+    my $struct = $param{struct}
+        or throw Avro::Schema::Error::ParseError("Fixed instantiation");
+
+    my $size = $struct->{size};
+    unless (defined $size && length $size) {
+        throw Avro::Schema::Error::ParseError("Fixed must declare 'size'");
+    }
+    if (ref $size) {
+        throw Avro::Schema::Error::ParseError(
+            "Fixed.size should be a scalar"
+        );
+    }
+    unless ($size =~ m{^\d+$} && $size > 0) {
+        throw Avro::Schema::Error::ParseError(
+            "Fixed.size should be a positive integer"
+        );
+    }
+    $schema->{size} = $size;
+
+    return $schema;
+}
+
+sub is_default_valid {
+    my $schema = shift;
+    my $default = shift;
+    my $size = $schema->{size};
+    return (1, $default) if $default && bytes::length $default == $size;
+    return (0);
+}
+
+sub size {
+    my $schema = shift;
+    return $schema->{size};
+}
+
+sub to_struct {
+    my $schema = shift;
+    my $known_names = shift || {};
+
+    my $fullname = $schema->fullname;
+    if ($known_names->{ $fullname }++) {
+        return $fullname;
+    }
+
+    return {
+        type => 'fixed',
+        name => $fullname,
+        size => $schema->{size},
+    };
+}
+
 
 package Avro::Schema::Error::ParseError;
 use parent 'Error::Simple';
--- t/01_schema.t
+++ t/01_schema.t
@@ -2,14 +2,18 @@ use strict;
 use warnings;
 
 use Test::More;
-plan tests => 22;
+plan tests => 88;
 use Test::Exception;
 use_ok 'Avro::Schema';
 
-dies_ok { Avro::Schema->new } "Should use parse() or instanciate the subclass";
+dies_ok { Avro::Schema->new } "Should use parse() or instantiate the subclass";
 
 throws_ok { Avro::Schema->parse(q()) } "Avro::Schema::Error::ParseError";
 throws_ok { Avro::Schema->parse(q(test)) } "Avro::Schema::Error::ParseError";
+throws_ok { Avro::Schema->parse(q({"type": t})) }
+            "Avro::Schema::Error::ParseError";
+throws_ok { Avro::Schema->parse(q({"type": t})) }
+            "Avro::Schema::Error::ParseError";
 
 my $s = Avro::Schema->parse(q("string"));
 isa_ok $s, 'Avro::Schema::Base';
@@ -21,84 +25,303 @@ isa_ok $s2, 'Avro::Schema::Primitive';
 is $s2->type, "string", "type is string";
 is $s, $s2, "string Schematas are singletons";
 
-my $s3 = Avro::Schema::Record->new(
-    struct => {
-        name => 'saucisson',
-        fields => [
-            { name => 'a', type => 'long'   },
-            { name => 'b', type => 'string' },
-        ],
-    },
-);
-
-isa_ok $s3, 'Avro::Schema::Record';
-is $s3->fullname, 'saucisson', "correct name";
-is $s3->fields->[0]{name}, 'a', 'a';
-is $s3->fields->[0]{type}, Avro::Schema::Primitive->new(type => 'long'), 'long';
-is $s3->fields->[1]{name}, 'b', 'b';
-is $s3->fields->[1]{type}, Avro::Schema::Primitive->new(type => 'string'), 'str';
-
-## self-refence
-$s3 = Avro::Schema::Record->new(
-    struct => {
-        name => 'saucisson',
-        fields => [
-            { name => 'a', type => 'long'   },
-            { name => 'b', type => 'saucisson' },
-        ],
-    },
-);
-isa_ok $s3, 'Avro::Schema::Record';
-is $s3->fullname, 'saucisson', "correct name";
-is $s3->fields->[0]{name}, 'a', 'a';
-is $s3->fields->[0]{type}, Avro::Schema::Primitive->new(type => 'long'), 'long';
-is $s3->fields->[1]{name}, 'b', 'b';
-is $s3->fields->[1]{type}, $s3, 'self!';
-
-done_testing;
-
-__DATA__
-{ "string" }
--> Avro::Schema::Primitive(string)
-
-{ "type": "string" }
--> Avro::Schema::Primitive(string)
-
-{ "type": "enum",
-  "name": "Suit",
-  "symbols" : ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
-}
--> Avro::Schema::Enum(name => suit, symbols = [])
-
+## Records
 {
-    "type": "record", 
-    "name": "test",
-    "fields" : [
-        {"name": "a", "type": "long"},
-        {"name": "b", "type": "string"}
-    ]
-}
--> Avro::Schema::Record(name => test, fields => [
-        { name => 'a', type => Avro::Schema::Primitive(long) }
-        { name => 'b', type => Avro::Schema::Primitive(string) }
-    ]
-)
+    my $s3 = Avro::Schema::Record->new(
+        struct => {
+            name => 'saucisson',
+            fields => [
+                { name => 'a', type => 'long'   },
+                { name => 'b', type => 'string' },
+            ],
+        },
+    );
 
+    isa_ok $s3, 'Avro::Schema::Record';
+    is $s3->type, 'record', "this is a record type";
+    is $s3->fullname, 'saucisson', "correct name";
+    is $s3->fields->[0]{name}, 'a', 'a';
+    is $s3->fields->[0]{type}, Avro::Schema::Primitive->new(type => 'long'), 'long';
+    is $s3->fields->[1]{name}, 'b', 'b';
+    is $s3->fields->[1]{type}, Avro::Schema::Primitive->new(type => 'string'), 'str';
+
+    ## self-reference
+    $s3 = Avro::Schema::Record->new(
+        struct => {
+            name => 'saucisson',
+            fields => [
+                { name => 'a', type => 'long'      },
+                { name => 'b', type => 'saucisson' },
+            ],
+        },
+    );
+    isa_ok $s3, 'Avro::Schema::Record';
+    is $s3->fullname, 'saucisson', "correct name";
+    is $s3->fields->[0]{name}, 'a', 'a';
+    is $s3->fields->[0]{type}, Avro::Schema::Primitive->new(type => 'long'), 'long';
+    is $s3->fields->[1]{name}, 'b', 'b';
+    is $s3->fields->[1]{type}, $s3, 'self!';
+
+    ## serialize
+    my $string = $s3->to_string;
+    like $string, qr/saucisson/, "generated string has 'saucisson'";
+    my $s3bis = Avro::Schema->parse($string);
+    is_deeply $s3bis, $s3, 'regenerated structure matches original';
+
+    ## record fields can have defaults
+    my @good_ints = (2, -1, -(2**31 - 1), 2_147_483_647, "2147483647"  );
+    my @bad_ints = ("", "string", 9.22337204, 9.22337204E10, 2**32 - 1,
+                    4_294_967_296, 9_223_372_036_854_775_807, \"2");
+    my @good_longs = (1, 2, -3, 9_223_372_036_854_775_807, 3e10);
+    my @bad_longs = (9.22337204, 9.22337204E10 + 0.1,
+                    9_223_372_036_854_775_808, \"2");
+
+    for (@good_ints) {
+        my $s4 = Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'int', default => $_ },
+                ],
+            },
+        );
+        is $s4->fields->[0]{default}, $_, "default $_";
+    }
+    for (@good_longs) {
+        my $s4 = Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'long', default => $_ },
+                ],
+            },
+        );
+        is $s4->fields->[0]{default}, $_, "default $_";
+    }
+    for (@bad_ints) {
+        throws_ok  { Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'int', default => $_ },
+                ],
+            },
+        ) } "Avro::Schema::Error::ParseError", "invalid default: $_";
+    }
+    for (@bad_longs) {
+        throws_ok  { Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'long', default => $_ },
+                ],
+            },
+        ) } "Avro::Schema::Error::ParseError", "invalid default: $_";
+    }
+
+    ## default of more complex types
+    throws_ok {
+        Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'union', default => 1 },
+                ],
+            },
+        )
+    } "Avro::Schema::Error::ParseError", "union don't have default: $@";
+
+    my $s4 = Avro::Schema->parse_struct(
+        {
+            type => 'record',
+            name => 'saucisson',
+            fields => [
+                { name => 'string', type => 'string', default => "something" },
+                { name => 'map', type => { type => 'map', values => 'long' }, default => {a => 2} },
+                { name => 'array', type => { type => 'array', items => 'long' }, default => [1, 2] },
+                { name => 'bytes', type => 'bytes', default => "something" },
+                { name => 'null', type => 'null', default => undef },
+            ],
+        },
+    );
+    is $s4->fields->[0]{default}, "something", "string default";
+    is_deeply $s4->fields->[1]{default}, { a => 2 }, "map default";
+    is_deeply $s4->fields->[2]{default}, [1, 2], "array default";
+    is $s4->fields->[3]{default}, "something", "bytes default";
+    is $s4->fields->[4]{default}, undef, "null default";
+    ## TODO: technically we should verify that default map/array match values
+    ## and items types defined
+
+    ## ordering
+    for (qw(ascending descending ignore)) {
+        my $s4 = Avro::Schema::Record->new(
+            struct => {
+                name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'int', order => $_ },
+                ],
+            },
+        );
+        is $s4->fields->[0]{order}, $_, "order set to $_";
+    }
+    for (qw(DESCEND ascend DESCENDING ASCENDING)) {
+        throws_ok  { Avro::Schema::Record->new(
+            struct => { name => 'saucisson',
+                fields => [
+                    { name => 'a', type => 'long', order => $_ },
+                ],
+            },
+        ) } "Avro::Schema::Error::ParseError", "invalid order: $_";
+    }
+}
+
+## Unions
 {
-  "type": "record", 
+    my $spec_example = <<EOJ;
+{
+  "type": "record",
   "name": "LongList",
   "fields" : [
-    {"name": "value", "type": "long"},             // each element has a long
-    {"name": "next", "type": ["LongList", "null"]} // optional next element
+    {"name": "value", "type": "long"},
+    {"name": "next", "type": ["LongList", "null"]}
   ]
 }
+EOJ
+    my $schema = Avro::Schema->parse($spec_example);
+    is $schema->type, 'record', "type record";
+    is $schema->fullname, 'LongList', "name is LongList";
 
--> Avro::Schema::Record(name => LongList, fields => [
-        { name => 'value', type => Avro::Schema::Primitive(long) }
-        { name => 'next', type => Avro::Schema::Union(fields => [
-            Avro::Schema::Named('LongList'),
-            Avro::Schema::Primitive(null)
-            ])
-        }
-    ]
-)
+    ## Union checks
+    # can only contain one type
+
+    $s = <<EOJ;
+["null", "null"]
+EOJ
+    throws_ok { Avro::Schema->parse($s) }
+              'Avro::Schema::Error::ParseError';
+
+    $s = <<EOJ;
+["long", "string", "float", "string"]
+EOJ
+    throws_ok { Avro::Schema->parse($s) }
+              'Avro::Schema::Error::ParseError';
+
+    $s = <<EOJ;
+{
+  "type": "record",
+  "name": "embed",
+  "fields": [
+    {"name": "value", "type":
+        { "type": "record", "name": "rec1",  "fields": [
+            { "name": "str1", "type": "string"}
+        ] }
+    },
+    {"name": "next", "type": ["embed", "rec1", "embed"] }
+  ]
+}
+EOJ
+    throws_ok { Avro::Schema->parse($s) }
+          'Avro::Schema::Error::ParseError',
+          'two records with same name in the union';
+
+    $s = <<EOJ;
+{
+  "type": "record",
+  "name": "embed",
+  "fields": [
+    {"name": "value", "type":
+        { "type": "record", "name": "rec1",  "fields": [
+            { "name": "str1", "type": "string"}
+        ] }
+    },
+    {"name": "next", "type": ["embed", "rec1"] }
+  ]
+}
+EOJ
+    lives_ok { Avro::Schema->parse($s) }
+             'two records of different names in the union';
+
+    # cannot directly embed another union
+    $s = <<EOJ;
+["long", ["string", "float"], "string"]
+EOJ
+    throws_ok { Avro::Schema->parse($s) }
+             'Avro::Schema::Error::ParseError', "cannot embed union in union";
+}
+
+## Enums!
+{
+    my $s = <<EOJ;
+{ "type": "enum", "name": "theenum", "symbols": [ "A", "B" ]}
+EOJ
+    my $schema = Avro::Schema->parse($s);
+    is $schema->type, 'enum', "enum";
+    is $schema->fullname, 'theenum', "fullname";
+    is $schema->symbols->[0], "A", "symbol A";
+    is $schema->symbols->[1], "B", "symbol B";
+    my $string = $schema->to_string;
+    my $s2 = Avro::Schema->parse($string);
+    is_deeply $s2, $schema, "reserialized identically";
+}
+
+## Arrays
+{
+    my $s = <<EOJ;
+{ "type": "array", "items": "string" }
+EOJ
+    my $schema = Avro::Schema->parse($s);
+    is $schema->type, 'array', "array";
+    is $schema->items, 'string', "type of items is string";
+    my $string = $schema->to_string;
+    my $s2 = Avro::Schema->parse($string);
+    is_deeply $s2, $schema, "reserialized identically";
+}
+
+## Maps
+{
+    my $s = <<EOJ;
+{ "type": "map", "values": "string" }
+EOJ
+    my $schema = Avro::Schema->parse($s);
+    is $schema->type, 'map', "map";
+    is $schema->values, 'string', "type of values is string";
+    my $string = $schema->to_string;
+    my $s2 = Avro::Schema->parse($string);
+    is_deeply $s2, $schema, "reserialized identically";
+}
+
+## Fixed
+{
+    my $s = <<EOJ;
+{ "type": "fixed", "name": "somefixed", "size": "something" }
+EOJ
+    throws_ok { Avro::Schema->parse($s) } "Avro::Schema::Error::ParseError",
+        "size must be an int";
+
+    $s = <<EOJ;
+{ "type": "fixed", "name": "somefixed", "size": -100 }
+EOJ
+    throws_ok { Avro::Schema->parse($s) } "Avro::Schema::Error::ParseError",
+        "size must be a POSITIVE int";
+
+    $s = <<EOJ;
+{ "type": "fixed", "name": "somefixed", "size": 0 }
+EOJ
+    throws_ok { Avro::Schema->parse($s) } "Avro::Schema::Error::ParseError",
+        "size must be a POSITIVE int > 0";
+
+    $s = <<EOJ;
+{ "type": "fixed", "name": "somefixed", "size": 0.2 }
+EOJ
+    throws_ok { Avro::Schema->parse($s) } "Avro::Schema::Error::ParseError",
+        "size must be an int";
+
+    $s = <<EOJ;
+{ "type": "fixed", "name": "somefixed", "size": 5e2 }
+EOJ
+    my $schema = Avro::Schema->parse($s);
+
+    is $schema->type, 'fixed', "fixed";
+    is $schema->fullname, 'somefixed', "name";
+    is $schema->size, 500, "size of fixed";
+    my $string = $schema->to_string;
+    my $s2 = Avro::Schema->parse($string);
+    is_deeply $s2, $schema, "reserialized identically";
+}
+
+done_testing;
